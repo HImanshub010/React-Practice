676edf31c627e5fa1a2e2954a4ec65ff6d77a718
HImanshub010 <himanshub010@gmail.com>
2020-11-19 23:18:52 +0530

    code to be refactored by useReducer

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -4,7 +4,42 @@ import { data } from '../../../data';
 // reducer function
 
 const Index = () => {
-  return <h2>useReducer</h2>;
+  const [name, setName] = useState('')
+  const [people, setPeople] = useState(data)
+  const [showModel, setShowModel] = useState(false)
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    if(name){
+      setShowModel(true)
+      setPeople((people) => [...people, {id: new Date().toString, name}])
+      setName('')
+    }else {
+      setShowModel(true)
+    }
+      setTimeout(() => {
+        setShowModel(false)
+      }, 3000);
+  }
+
+  return <>
+  {showModel && <Modal/>}
+    <form className='form' onSubmit={handleSubmit}>
+      <input type="text" value = {name} onChange={(e) => setName(e.target.value)}/>
+      <button type='submit'>Add People</button>
+    </form>
+    {
+      people.map((person) => {
+        return (
+          <div key={person.id}>
+            <h4>{person.name}</h4>
+          </div>
+        )
+      })
+    }
+  </>
+  ;
 };
 
-export default Index;
+export default Index
+ 
