8e193f8e4d663d55824e8ef6f162ebf6d96c2a07
HImanshub010 <himanshub010@gmail.com>
2020-11-20 00:20:15 +0530

    completed useReducer

--- src/tutorial/6-useReducer/setup/Modal.js
+++ src/tutorial/6-useReducer/setup/Modal.js
@@ -1,7 +1,13 @@
 import React, { useEffect } from 'react';
 
-const Modal = () => {
-  return <div>i'm modal</div>;
+const Modal = ({modelContent, closeModel}) => {
+  setTimeout(() => {
+    closeModel()
+  }, 3000);
+  
+  return <div className='model'>
+    <p>{modelContent}</p>
+  </div>;
 };
 
 export default Modal;
--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -3,43 +3,85 @@ import Modal from './Modal';
 import { data } from '../../../data';
 // reducer function
 
+const reducer = (state, action) => {
+  if(action.type === 'ADD_ITEM'){
+    const newPeople = [...state.people, action.payload]
+    console.log(newPeople)
+    return{
+      ...state,
+      people: newPeople,
+      isModelOpen: true,
+      modelContent: 'NEW ITEM ADDED'
+    }
+  }
+  if (action.type === 'CLOSE_MODEL'){
+    return {
+      ...state,
+      isModelOpen: false
+    }
+  }
+  if (action.type === 'NO_ITEM_FOUND'){
+  return {
+    ...state,
+    isModelOpen: true,
+    modelContent: 'Empty Value Found'
+    }
+  }
+
+  if(action.type === 'REMOVE_ITEM'){
+    const newPeople = state.people.filter((person) => person.id !== action.payload)
+    return{
+      ...state,
+      people:newPeople
+    }
+  }
+  throw new Error('No ACTION FOUND FOR REDUCER')
+}
+
+const defaultState = {
+  people : [],
+  isModelOpen : false,
+  modelContnet: ''
+}
+
 const Index = () => {
   const [name, setName] = useState('')
-  const [people, setPeople] = useState(data)
-  const [showModel, setShowModel] = useState(false)
+  const [state, dispatch] = useReducer(reducer, defaultState)
 
   const handleSubmit = (e) =>{
     e.preventDefault();
     if(name){
-      setShowModel(true)
-      setPeople((people) => [...people, {id: new Date().toString, name}])
-      setName('')
+     dispatch({type:'ADD_ITEM', payload: {id: new Date().getTime().toString(), name}})
+     setName('')
     }else {
-      setShowModel(true)
-    }
-      setTimeout(() => {
-        setShowModel(false)
-      }, 3000);
+      dispatch({type: 'NO_ITEM_FOUND'})
   }
+}
 
+const closeModel = () =>{
+  dispatch({type:'CLOSE_MODEL'})
+}
   return <>
-  {showModel && <Modal/>}
+  {state.isModelOpen && <Modal 
+  modelContent={state.modelContent}
+  closeModel={closeModel}
+  />
+  }
     <form className='form' onSubmit={handleSubmit}>
       <input type="text" value = {name} onChange={(e) => setName(e.target.value)}/>
       <button type='submit'>Add People</button>
     </form>
     {
-      people.map((person) => {
+      state.people.map((person) => {
         return (
           <div key={person.id}>
             <h4>{person.name}</h4>
+            <button onClick = {()=> dispatch({type:'REMOVE_ITEM', payload: person.id})}> Remove </button>
           </div>
         )
       })
     }
-  </>
-  ;
+  </>;
 };
 
 export default Index
- 
